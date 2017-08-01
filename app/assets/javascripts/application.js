fb5e242557cb72db6f64f467f9698018385f3f59
Ethan Harris <ethan17458@gmail.com>
2017-07-31 20:01:07 -0400

    removed js debug message

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -59,7 +59,6 @@ var color2 = "rgb("+r2+","+g2+","+b2+")";
  $('#gradient').css({
    background: "-webkit-gradient(linear, left top, right top, from("+color1+"), to("+color2+"))"}).css({
     background: "-moz-linear-gradient(left, "+color1+" 0%, "+color2+" 100%)"});
-  console.log("background updated");
   step += gradientSpeed;
   if ( step >= 1 )
   {
